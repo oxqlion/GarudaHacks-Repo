98b726d898bb9172fc42677ead05920af9bd4742
hayyaoe <hayya947@gmail.com>
2024-07-12 16:11:26 +0700

    progress for changing laptop

--- client/src/firebaseConfig.js
+++ client/src/firebaseConfig.js
@@ -1,18 +1,18 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getFirestore } from "firebase/firestore"
+import { getFirestore } from "firebase/firestore";
 import { getAuth } from "firebase/auth";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "",
-  authDomain: "",
-  projectId: "",
-  storageBucket: "",
-  messagingSenderId: "",
-  appId: ""
+  apiKey: "AIzaSyA6ZEt_D0ML7UOSYCr26UvwCAhPj9yr2Ww",
+  authDomain: "convy-565c5.firebaseapp.com",
+  projectId: "convy-565c5",
+  storageBucket: "convy-565c5.appspot.com",
+  messagingSenderId: "906417436548",
+  appId: "1:906417436548:web:b84062e5655f83f0270fd1",
 };
 
 // Initialize Firebase
