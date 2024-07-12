805a026329bd517ee0e4ded1b09721a3f9d6fe72
hayyaoe <hayya947@gmail.com>
2024-07-12 10:46:35 +0700

    login view done with some minor bug

--- client/src/LoginView.jsx
+++ client/src/LoginView.jsx
@@ -1,52 +1,73 @@
-import React, { useState } from 'react';
-import { fb_auth } from './firebaseConfig';
-import { signInWithEmailAndPassword } from 'firebase/auth';
-import './index.css'
+import React, { useState } from "react";
+import { fb_auth } from "./firebaseConfig";
+import { signInWithEmailAndPassword } from "firebase/auth";
+import "./index.css";
 const Login = () => {
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
-  const [error, setError] = useState('');
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState("");
 
   const handleLogin = async (e) => {
     e.preventDefault();
-    setError('');
+    setError("");
 
     try {
       await signInWithEmailAndPassword(fb_auth, email, password);
-      alert('Logged in successfully!');
+      alert("Logged in successfully!");
     } catch (error) {
       setError(error.message);
     }
   };
 
   return (
-    <div className='h-screen w-screen'>
-      <h2 className='font-bold text-2xl w-full text-center font-inter '>Login</h2>
-      <form onSubmit={handleLogin}>
-        <div>
-          <label>Email:</label>
+    <div className="flex h-screen w-screen items-center justify-center">
+      <div>
+      <h2 className="font-bold text-2xl w-full text-center font-inter ">
+        Login
+      </h2>
+        <div className="w-full px-12 py-2">
+          <p className="font-md text-sm w-full text-left font-inter mb-2">
+            Email
+          </p>
           <input
+            className="w-full font-regular rounded-xl border-2 border-black bg-text-box-bg text-lg py-3 px-2"
             type="email"
             value={email}
             onChange={(e) => setEmail(e.target.value)}
             required
           />
         </div>
-        <div>
-          <label>Password:</label>
+        <div className="w-full px-12 py-2">
+          <p className="font-md text-sm w-full text-left font-inter mb-2">
+            Password
+          </p>
           <input
+            className="w-full font-regular rounded-xl border-2 border-black bg-text-box-bg text-lg py-3 px-2"
             type="password"
             value={password}
             onChange={(e) => setPassword(e.target.value)}
             required
           />
         </div>
-        <button type="submit">Login</button>
-        {error && <p style={{ color: 'red' }}>{error}</p>}
-      </form>
+        <div className="w-full px-12 py-1">
+          <div className="flex flex-row mb-2">
+            <input type="checkbox" id="agree" name="agree" />
+            <p className="text-sm font-inter px-2">
+              I agree to the terms & services
+            </p>
+          </div>
+          <button
+            onClick={handleLogin}
+            className="w-full rounded-full py-2 border-2 bg-button-color text-white text-xl font-semibold"> Login
+          </button>
+
+        {error && <p className="text-red-500 py-2 w-full text-center">{error}</p>}
+    <p className="py-2 w-full text-center font-semibold text-sm">Don't have an account? <a className="text-blue-500">
+    Register </a> Here</p>
+        </div>
+    </div>
     </div>
   );
 };
 
 export default Login;
-
--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,16 +1,16 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,jsx,ts,tsx}",
-  ],
+  content: ["./index.html", "./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
     extend: {
       fontFamily: {
-        "inter": ["Inter", "sans-serif"]
+        inter: ["Inter", "sans-serif"],
       },
+    },
+    colors: {
+      "text-box-bg": "#ECECEC",
+      "button-color": "#6270A0",
     },
   },
   plugins: [],
-}
-
+};
