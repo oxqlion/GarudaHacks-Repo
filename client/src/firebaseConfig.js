7b762dcce14089341c3efa5be3ccbdcfe3aa4fb9
hayyaoe <hayya947@gmail.com>
2024-07-13 00:46:23 +0700

    create course view fixed

--- client/src/firebaseConfig.js
+++ client/src/firebaseConfig.js
@@ -2,7 +2,6 @@
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 import { getAuth } from "firebase/auth";
-import { getStorage } from "firebase/storage";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -20,6 +19,5 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
 const fb_auth = getAuth(app);
-const storage = getStorage(app);
 
-export { db, fb_auth, storage };
+export { db, fb_auth };
